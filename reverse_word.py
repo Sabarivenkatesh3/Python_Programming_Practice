9ea67a2afa820a6aec17568ce5f7b0309c0230b6
sabarivarun3 <sabarivarun3@gmail.com>
2025-09-29 09:16:45 +0530

    feat: new method was added using sys.arg

--- reverse_word.py
+++ reverse_word.py
@@ -11,3 +11,22 @@ print("reversed word: ", reverse_word)
 
 for j in reverse_word:
     print(j)
+
+### to use another method
+"""import sys
+
+word = " ".join(sys.argv[1:])
+
+
+reverse_word = []
+
+for i in word:
+    print(i)
+
+reverse_word = word[::-1]
+
+print("reversed word: ", reverse_word)
+
+for j in reverse_word:
+    print(j)
+"""
