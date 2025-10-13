bb9c63cf0d23310ea7b44dafe056745dfc685059
sabarivarun3 <sabarivarun3@gmail.com>
2025-10-13 22:11:22 +0530

    codes added

---
+++ codes/abc.py
@@ -0,0 +1,7 @@
+num = list(map(int, input().split()))
+
+num.sort(reverse=True)
+l_3 = num[0:3]
+
+print(num)
+print(l_3)
---
+++ codes/find_A_string.py
@@ -0,0 +1,14 @@
+s1 = input()
+s2 = input()
+
+set1 = set(s1)
+set2 = set(s2)
+
+print(set1)
+print(set2)
+
+dif1 = set1 - set2
+dif2 = set2 - set1
+
+print(dif1)
+print(dif2)
